9b62e81c239bd6c92bf826d51138cfdc7a396adf
hesham1010 <hesham.gublootech@gmail.com>
2024-05-28 22:09:30 +0500

    changes

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
+const path = require("path");
+
 const express = require("express");
+const cookieParser = require("cookie-parser");
+
+const db = require("./config/mongoose-connection");
+const ownersRouter = require("./routes/ownersRouter");
+const usersRouter = require("./routes/usersRouter");
+const productsRouter = require("./routes/productsRouter");
 
 const app = express();
-app.use((req, res, next) => {
-  console.log("in the middleware");
-  next();
-});
-app.use("/", (req, res, next) => {
-  console.log("in the middleware");
-  res.send("<h1>hello from express</h1>");
-  next();
-});
-app.use("/add-product", (req, res, next) => {
-  console.log("in the middleware");
-  res.send("<h1>Add Products</h1>");
-});
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view-engine", "ejs");
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
 app.listen(3000);
